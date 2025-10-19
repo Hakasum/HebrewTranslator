51bdd6b8d7114f3a2b707305f4fadd0daf7d8f09
Hakasum <130662713+Hakasum@users.noreply.github.com>
2025-10-19 14:29:59 +0300

    Added port suitable for render

--- app.py
+++ app.py
@@ -122,7 +122,6 @@ if __name__ == '__main__':
     print("Starting Hebrew Translator API...")
     initialize_translator()
     
-    # Run the Flask app
-    port = int(os.environ.get('PORT', 5005))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)  # debug=False in production
 
